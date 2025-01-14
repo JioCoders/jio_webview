bc4765335f3091063b2af589b79307cd0e3cdfa0
Amit Kumar <amit2.kumar.cedge@sbi.co.in>
2025-01-14 11:18:03 +0530

    pod_file_update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -21,7 +21,7 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    const webUrl = 'https://iocode.shop/';
+    const webUrl = 'https://flutter.dev/';
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
